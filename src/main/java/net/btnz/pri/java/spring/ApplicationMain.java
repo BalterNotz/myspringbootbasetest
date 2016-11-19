c6bcc3c449f6997e79c04848fbdc1daaddd46609
Balter Notz <btnz@foxmail.com>
2016-11-19 14:54:01 +0800

    first run

--- src/main/java/net/btnz/pri/java/spring/ApplicationMain.java
+++ src/main/java/net/btnz/pri/java/spring/ApplicationMain.java
@@ -1,13 +1,18 @@
 package net.btnz.pri.java.spring;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
  * Created by zhangsongwei on 2016/11/18.
  */
+@SpringBootApplication
 public class ApplicationMain {
-    public static void main(String[] argvs) {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationMain.class, args);
         System.out.println("Application started at " + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(new Date()));
     }
 }
