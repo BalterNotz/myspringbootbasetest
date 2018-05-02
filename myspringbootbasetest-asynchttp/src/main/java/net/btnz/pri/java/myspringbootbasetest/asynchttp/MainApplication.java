d4869ca7af9ea4eade6c4e3d6071e4de7028f63a
BalterNotz <BalterNotz@foxmail.com>
2018-05-02 11:57:21 +0800

    temporary commit

---
+++ myspringbootbasetest-asynchttp/src/main/java/net/btnz/pri/java/myspringbootbasetest/asynchttp/MainApplication.java
@@ -0,0 +1,12 @@
+package net.btnz.pri.java.myspringbootbasetest.asynchttp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String...args) {
+        SpringApplication.run(MainApplication.class);
+        System.out.println("Hello world!");
+    }
+}
---
+++ myspringbootbasetest-canalotter/src/main/java/net/btnz/pri/java/myspringbootbasetest/canalotter/MainApplication.java
@@ -0,0 +1,4 @@
+package net.btnz.pri.java.myspringbootbasetest.canalotter;
+
+public class MainApplication {
+}
